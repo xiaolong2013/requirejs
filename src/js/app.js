3980650148bb7e03c7fa3a4bfeda7978f40cef13
yangxiaolong <yangxiaolong@fanghuwang.com>
2018-01-16 19:34:15 +0800

    requirejs

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+
+//设置配置
+requirejs.config({
+    //默认的模块 从 js/lib 中加载
+    baseUrl : 'js/lib',
+    //如果模块以 app开头的话 则 从 js/app下加载
+    //因为 app 相对于 baseUrl   
+    paths : {
+    	app : "../app"
+    }
+})
+
+
+//以下的 jquery canvas 都不需要加路径 可以默认baseUrl 中加载
+//sub 默认从
+requirejs(['jquery', 'canvas', 'app/sub'], function($, canvas, sub){
+     
+    //加载进来 jquery 
+    $(function(){
+         alert("domReady");
+    })
+    
+    console.log("$====="+$);
+    console.log("color=="+canvas.color);
+    console.log("size==="+sub.size);
+})
